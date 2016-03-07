d8af8c2c42278c7201304450832436b0ffa2c456
vain0 <id_vain@yahoo.co.jp>
2016-03-07 22:28:46 +0900

    `T5` has `RequireQualifiedAccessAttribute`

--- ZeoFive/Util.fs
+++ ZeoFive/Util.fs
@@ -8,6 +8,7 @@ module Misc =
   type T5<'T> =
     'T * 'T * 'T * 'T * 'T
 
+[<RequireQualifiedAccess>]
 module T5 =
   let zip (x0, x1, x2, x3, x4) (y0, y1, y2, y3, y4) =
     ((x0, y0), (x1, y1), (x2, y2), (x3, y3), (x4, y4))
