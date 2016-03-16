5a9f89d92bea46d06c0e15f7c2a45fb850b1fc45
vain0 <id_vain@yahoo.co.jp>
2016-03-16 17:56:56 +0900

    [util] Add `serializeJson` and `deserializeJson`

--- ZeoFive/Util.fs
+++ ZeoFive/Util.fs
@@ -263,3 +263,25 @@ module Observable =
       error err
 
     member this.AsObservable = obs
+
+module Serialize =
+  open System.Text
+  open System.Runtime.Serialization
+  open System.Runtime.Serialization.Json
+
+  // https://gist.github.com/theburningmonk/2071722
+  let private toString = Encoding.UTF8.GetString
+  let private toBytes (x: string) = Encoding.UTF8.GetBytes x
+
+  let serializeJson<'a> (x : 'a) = 
+    let jsonSerializer = new DataContractJsonSerializer(typedefof<'a>)
+
+    use stream = new IO.MemoryStream()
+    jsonSerializer.WriteObject(stream, x)
+    toString <| stream.ToArray()
+
+  let deserializeJson<'a> (json : string) =
+    let jsonSerializer = new DataContractJsonSerializer(typedefof<'a>)
+
+    use stream = new IO.MemoryStream(toBytes json)
+    jsonSerializer.ReadObject(stream) :?> 'a
